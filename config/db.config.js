df3353c917f81175249f0da6cae13a2f6b7a159b
Ayman Ishimwe <ishimweay@mtn.co.rw>
2020-05-04 17:01:22 +0200

    Set real database parameters

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb",
+    PASSWORD: "data234$",
+    DB: "bananaDB",
     dialect: "mysql",
     pool: {
         max: 5,
